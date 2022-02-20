b9a7fd17c1b57e46966656bb3228434f3a69a2a8
AJ Rogers <adam.j.rogers.89@gmail.com>
2022-02-20 15:19:34 -0600

    Rock-paper-scissors rough draft

---
+++ rps.js
@@ -0,0 +1,61 @@
+function computerTurn() {
+    let randomNumber = Math.floor(Math.random() * 3);
+    if (randomNumber === 0) {
+        randomNumber = "Rock";
+        console.log("Rock");
+    } else if (randomNumber === 1) {
+        randomNumber = "Paper";
+        console.log("Paper");
+    } else {
+        randomNumber = "Scissors";
+        console.log("Scissors");
+    }
+}
+
+function playerTurn() {
+    let userPlay = prompt("Please input your choice of: Rock, paper, or scissors: ");
+    userPlayFL = userPlay.charAt(0).toUpperCase();
+    userPlayR = userPlayFL + userPlay.slice(1).toLowerCase();
+    console.log(userPlayR);
+}
+
+
+/*
+function rps(playerSelection, computerSelection) {
+    if (playerSelection === "Rock" && computerSelection === 0) {
+        console.log("It's a tie!");
+    } else if (playerSelection === "Paper" && computerSelection === 0) {
+        console.log("You win! Paper beats Rock!");
+    } else if (playerSelection === "Scissors" && computerSelection === 0) {
+        console.log("You lose! Rock beats paper!");
+    } else if (playerSelection === "Paper" && computerSelection === 1) {
+        console.log("It's a tie!");
+    } else if (playerSelection === "Scissors" && computerSelection === 1) {
+        console.log("You win! Scissors beats Paper!");
+    } else if (playerSelection === "Rock" && computerSelection === 1) {
+        console.log("You lose! Paper beats Rock!");
+    } else if (playerSelection === "Scissors" && computerSelection === 2) {
+        console.log("It's a tie!");
+    } else if (playerSelection === "Rock" && computerSelection === 2) {
+        console.log("You win! Rock beats Scissors!");
+    } else if (playerSelection === "Paper" && computerSelection === 2) {
+        console.log("You lose! Scissors beats Paper!")
+    }
+} 
+
+rps(playerSelection, computerSelection);
+*/
+
+function example() {
+    const playerSelection = playerTurn();
+    const computerSelection = computerTurn();
+    if (playerTurn() === "Rock") {
+        console.log("The computer played: Rock");
+    } else if (playerTurn() === "Scissors") {
+        console.log("The computer played: Scissors");
+    } else if (playerTurn() === "Paper") {
+        console.log("The computer played: Paper");
+    }
+}
+
+example();
