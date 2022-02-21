5783cf7e067a2fe36e861a9c87f76423f4db7583
AJ Rogers <adam.j.rogers.89@gmail.com>
2022-02-20 21:40:33 -0600

    Updated functions to display correct output.

--- rps.js
+++ rps.js
@@ -58,40 +58,87 @@ function oneGame() {
 }
 */
 
+const playerSelection = playerPlay();
+const computerSelection = computerPlay();
+
 function oneGame(playerSelection, computerSelection) {
     let result = ''
+    let playerPoints = 0
+    let computerPoints = 0
     if (playerSelection === "Rock" && computerSelection == "Rock") {
+        console.log("Player chose: " + playerSelection);
+        console.log("Computer chose: " + computerSelection);
         result = "It's a tie!"
         return result;
     } else if (playerSelection === "Rock" && computerSelection === "Scissors") {
+        playerPoints++
+        console.log("Player chose: " + playerSelection);
+        console.log("Computer chose: " + computerSelection);
         result = "Player wins! Rock beats Scissors."
+        console.log("Player points: " + playerPoints);
+        console.log("Computer points: " + computerPoints);
         return result;
     } else if (playerSelection === "Rock" && computerSelection === "Paper") {
+        computerPoints++
+        console.log("Player chose: " + playerSelection);
+        console.log("Computer chose: " + computerSelection);
         result = "Computer wins! Paper beats Rock."
+        console.log("Player points: " + playerPoints);
+        console.log("Computer points: " + computerPoints);
         return result;
+        break;
     } else if (playerSelection === "Paper" && computerSelection === "Scissors") {
+        computerPoints++
+        console.log("Player chose: " + playerSelection);
+        console.log("Computer chose: " + computerSelection);
         result = "Computer wins! Scissors beats Paper."
+        console.log("Player points: " + playerPoints);
+        console.log("Computer points: " + computerPoints);
         return result;
     } else if (playerSelection === "Paper" && computerSelection === "Rock") {
+        playerPoints++
+        console.log("Player chose: " + playerSelection);
+        console.log("Computer chose: " + computerSelection);
         result = "Player wins! Paper beats Rock."
+        console.log("Player points: " + playerPoints);
+        console.log("Computer points: " + computerPoints);
         return result;
     } else if (playerSelection === "Scissors" && computerSelection === "Paper") {
+        playerPoints++
+        console.log("Player chose: " + playerSelection);
+        console.log("Computer chose: " + computerSelection);
         result = "Player wins! Scissors beats Paper."
+        console.log("Player points: " + playerPoints);
+        console.log("Computer points: " + computerPoints);
         return result;
     } else if (playerSelection === "Scissors" && computerSelection === "Rock") {
+        computerPoints++
+        console.log("Player chose: " + playerSelection);
+        console.log("Computer chose: " + computerSelection);
         result = "Computer wins! Rock beats Scissors."
+        console.log("Player points: " + playerPoints);
+        console.log("Computer points: " + computerPoints);
         return result;
     } else if (playerSelection === "Paper" && computerSelection === "Paper") {
+        console.log("Player chose: " + playerSelection);
+        console.log("Computer chose: " + computerSelection);
         result = "It's a tie!"
         return result;
     } else if (playerSelection === "Scissors" && computerSelection === "Scissors") {
+        console.log("Player chose: " + playerSelection);
+        console.log("Computer chose: " + computerSelection);
         result = "It's a tie!"
         return result;
     }
 }
 
 
-console.log(oneGame());
+function game() {
+    for (let i = 0; i < 5; i++) {
+        console.log(oneGame(playerSelection, computerSelection));
+    }
+}
 
+console.log(game());
 
 
